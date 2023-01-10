3ef825f542c4029cbea9a12a6268e58ae5598ee0
kushjayz <93889509+kushjayz@users.noreply.github.com>
2023-01-10 05:46:30 +0530

    Update settings.py
    Config changes

--- NSSLSite/settings.py
+++ NSSLSite/settings.py
@@ -82,6 +82,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'new': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nssllkdb',
+        'USER': 'nssllk',
+        'PASSWORD': 'Mwa97iAOdQrg',
+        'HOST': '51.79.249.39',
+        'PORT': '22',
     }
 }
 
